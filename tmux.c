c3c08d0140d84ca25603511eaa3db0c3fe312a59
Leonardo Romor <leonardo.romor@gmail.com>
2023-07-17 09:17:39 +0200

    added fixes to allow latest head to compile and missing tty term code

--- tmux.c
+++ tmux.c
@@ -141,7 +141,7 @@ make_label(const char *label, char **cause)
 #if __MSYS__ || __CYGWIN__ || _WIN32 || _WIN64
         if (sb.st_uid != uid) {
 #else
-        if (sb.st_uid != uid || (sb.st_mode & PERMS) != 0) {
+        if (sb.st_uid != uid) {
 #endif
 		errno = EACCES;
 		goto fail;
